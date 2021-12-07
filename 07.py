93da89ba726d28f290de05025ba32e159b022ca7
Anže Pečar <anze@pecar.me>
2021-12-07 08:13:09 +0100

    Solve day 7

---
+++ 07.py
@@ -0,0 +1,28 @@
+from rich.progress import track
+import math
+import statistics
+from dataclasses import dataclass
+from collections import Counter, defaultdict
+from utils import get_input, submit, console
+
+
+inp = get_input(7, no_split=True)
+# inp = """16,1,2,0,4,2,7,1,2,14"""
+inp = [int(i) for i in inp.split(",")]
+target_pos = statistics.median(inp)
+answer = int(sum([abs(i - target_pos) for i in inp]))
+console.log(f"{answer}")
+
+mn, mx = min(inp), max(inp)
+console.log(mn, mx)
+min_fuel = math.inf
+t = None
+for target_pos in track(range(mn, mx)):
+    fuel = 0
+    for crab in inp:
+        distance = abs(crab - target_pos)
+        fuel += sum([i for i in range(distance + 1)])
+    if fuel < min_fuel:
+        min_fuel = fuel
+        t = target_pos
+console.log(min_fuel, t)
